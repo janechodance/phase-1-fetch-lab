26c47907ecf3eb510e470ec198e7b390f9b77896
Jane <janechodance@gmail.com>
2021-09-28 14:07:43 -0400

    commit tree 7d80cba5c594e6d1fc8419962218efcb5f4291fb

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const item = fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(function(json){
+    return renderBooks(json);
+  })
+  return item
 }
-
+  
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
